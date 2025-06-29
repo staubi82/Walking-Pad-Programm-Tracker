4bf291d99700f27c1d3a7426bb48e31625c889c7
Staubi <56784126+staubi82@users.noreply.github.com>
2025-06-29 18:12:52 +0200

    MIME-Type Problem beheben

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,22 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  server: {
+    port: 5173,
+    host: true
+  },
+  preview: {
+    port: 3814,
+    host: true
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
